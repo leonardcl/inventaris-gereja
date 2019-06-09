a3f1149849ab08acb79fc8fb4a8a61d88fa954fc
leonardcl <50519333+leonardcl@users.noreply.github.com>
2019-06-08 23:22:10 -0700

    test grafik php

--- graph.php
+++ graph.php
@@ -1,44 +1,54 @@
-<?php 
+
+
+
+<?php
+ 
 $dataPoints = array();
-    include 'connect.php';
-    $no = 1;
+//Best practice is to create a separate file for handling connection to database
+try{
+     // Creating a new connection.
+    // Replace your-hostname, your-db, your-username, your-password according to your database
+    include('connect.php');
     $data = mysqli_query($conn,"SELECT * FROM peminjaman group by id_barang");
     while($d = mysqli_fetch_array($data)){
-    ?>
-        <tr>
-            
-            <?php
-            $idbrng = $d['id_barang'];
-            echo "<td>",$d['id_barang'],"</td>";
-            $data2= mysqli_query($conn, "SELECT count(id_barang) FROM peminjaman where id_barang = $idbrng ");
-            $d2 = mysqli_fetch_array($data2);
-            echo "<td>",$d2[0],"</td>";
-            $nilai = array("y" => $d2, "label" => $idbrng);
-            array_push($dataPoints, $nilai);
-            ?>
-        </tr>
-        <?php 
-    }
-    ?>
 
+        $idbrng = $d['id_barang'];
+        $id_brng = $d['id_barang'];
+							
+								$data1 = mysqli_query($conn,"select nama_barang from barang where id = $id_brng");
+								$d1 = mysqli_fetch_array($data1);
+        $data2= mysqli_query($conn, "SELECT count(id_barang) FROM peminjaman where id_barang = $idbrng ");
+        $d2 = mysqli_fetch_array($data2);
+        array_push($dataPoints, array("label" => $d1['nama_barang'], "y" => $d2[0]));
+ $conn = null;
+}
+}
+
+
+
+
+
+
+catch(\PDOException $ex){
+    print($ex->getMessage());
+}
+	
+?>
 <!DOCTYPE HTML>
 <html>
-<head>
+<head>  
 <script>
-window.onload = function() {
+window.onload = function () {
  
 var chart = new CanvasJS.Chart("chartContainer", {
 	animationEnabled: true,
-	theme: "light2",
+	exportEnabled: true,
+	theme: "light1", // "light1", "light2", "dark1", "dark2"
 	title:{
-		text: "Gold Reserves"
-	},
-	axisY: {
-		title: "Gold Reserves (in tonnes)"
+		text: "PHP Column Chart from Database"
 	},
 	data: [{
-		type: "column",
-		yValueFormatString: "#,##0.## tonnes",
+		type: "column", //change type to bar, line, area, pie, etc  
 		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
 	}]
 });
@@ -51,4 +61,4 @@ chart.render();
 <div id="chartContainer" style="height: 370px; width: 100%;"></div>
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 </body>
-</html>       
+</html>      
--- report.php
+++ report.php
@@ -1,3 +1,35 @@
+<?php 
+$dataPoints = array();
+//Best practice is to create a separate file for handling connection to database
+try{
+     // Creating a new connection.
+    // Replace your-hostname, your-db, your-username, your-password according to your database
+    include('connect.php');
+    $data = mysqli_query($conn,"SELECT * FROM history group by id_barang");
+    while($d = mysqli_fetch_array($data)){
+
+        $idbrng = $d['id_barang'];
+        $id_brng = $d['id_barang'];
+							
+								$data1 = mysqli_query($conn,"select nama_barang from barang where id = $id_brng");
+								$d1 = mysqli_fetch_array($data1);
+        $data2= mysqli_query($conn, "SELECT count(id_barang) FROM peminjaman where id_barang = $idbrng ");
+        $d2 = mysqli_fetch_array($data2);
+        array_push($dataPoints, array("label" => $d1['nama_barang'], "y" => $d2[0]));
+ $conn = null;
+}
+}
+
+
+
+
+
+
+catch(\PDOException $ex){
+    print($ex->getMessage());
+}
+	
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,7 +39,25 @@
     <title>Tabel Peminjaman</title>
 
     <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
-
+		<script>
+window.onload = function () {
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	exportEnabled: true,
+	theme: "light1", // "light1", "light2", "dark1", "dark2"
+	title:{
+		text: "Jumlah Peminjaman"
+	},
+	data: [{
+		type: "column", //change type to bar, line, area, pie, etc  
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+ 
+}
+</script>
 </head>
 <body>
 
@@ -42,6 +92,7 @@
 							$data2= mysqli_query($conn, "SELECT count(id_barang) FROM peminjaman where id_barang = $idbrng ");
 							$d2 = mysqli_fetch_array($data2);
 							echo "<td>",$d2[0],"</td>";
+
 							?>
 						</tr>
 						<?php 
@@ -51,7 +102,8 @@
 			</div>
 		</div>
 	</div>
-	
+	<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>

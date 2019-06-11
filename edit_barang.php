f6486fec66002a686dd2cc7ae9add5013e063559
leonardcl <50519333+leonardcl@users.noreply.github.com>
2019-06-11 09:52:52 -0700

    offline bootstrap

--- edit_barang.php
+++ edit_barang.php
@@ -136,10 +136,7 @@ while($user_data = mysqli_fetch_array($result))
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Edit Barang</title>
-    <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
-    <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>
-    <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script>
+    <link rel='stylesheet' href='resource/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
 </head>
 <body>
     <?php 
@@ -232,11 +229,12 @@ while($user_data = mysqli_fetch_array($result))
   </div>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<script type='text/javascript' src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/js/bootstrap-datepicker.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker3.css">
+<script src="resource/jquery-3.3.1.slim.min.js" ></script>
+<script src="resource/popper.min.js" ></script>
+<script src="resource/bootstrap.min.js" ></script>
+<script src="resource/bootstrap.bundle.min.js"></script>
+<script type='text/javascript' src="resource/bootstrap-datepicker.min.js"></script>
+<link rel="stylesheet" href="resource/bootstrap-datepicker3.css">
 <script type='text/javascript'>
 
     $('.date').datepicker({
--- report.php
+++ report.php
@@ -38,7 +38,7 @@ catch(\PDOException $ex){
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tabel Peminjaman</title>
 
-    <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
+    <link rel='stylesheet' href='resource/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
 		<script>
 window.onload = function () {
  
@@ -72,10 +72,11 @@ chart.render();
     	</div>	
 	</div>
 		<div class="row">
-			<div class="col-12">
+		<div class="col-3"></div>
+			<div class="col-6">
 				<table class='table table-hover'>
 					<tr class="bg-info">
-						<th>ID Barang</th>
+						<th>Nama Barang</th>
 						<th>Jumlah Peminjaman (x)</th>
 					</tr>
 					<?php 
@@ -88,7 +89,9 @@ chart.render();
 							
 							<?php
 							$idbrng = $d['id_barang'];
-							echo "<td>",$d['id_barang'],"</td>";
+							$data1 = mysqli_query($conn,"select nama_barang from barang where id = $idbrng");
+								$d1 = mysqli_fetch_array($data1);
+							echo "<td>",$d1['nama_barang'],"</td>";
 							$data2= mysqli_query($conn, "SELECT count(id_barang) FROM peminjaman where id_barang = $idbrng ");
 							$d2 = mysqli_fetch_array($data2);
 							echo "<td>",$d2[0],"</td>";
@@ -103,12 +106,13 @@ chart.render();
 		</div>
 	</div>
 	<div id="chartContainer" style="height: 370px; width: 100%;"></div>
-<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<script type='text/javascript' src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/js/bootstrap-datepicker.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker3.css">
+<script src="resource/canvasjs.min.js"></script>
+<script src="resource/jquery-3.3.1.slim.min.js" ></script>
+<script src="resource/popper.min.js" ></script>
+<script src="resource/bootstrap.min.js" ></script>
+<script src="resource/bootstrap.bundle.min.js"></script>
+<script type='text/javascript' src="resource/bootstrap-datepicker.min.js"></script>
+<link rel="stylesheet" href="resource/bootstrap-datepicker3.css">
 <script type='text/javascript'>
 
     $('.date').datepicker({

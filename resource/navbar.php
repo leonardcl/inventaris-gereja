50d4cb9a799c7fe3987d8b92cae8516f44566b68
leonardcl <50519333+leonardcl@users.noreply.github.com>
2019-06-11 09:58:04 -0700

    no home

--- resource/navbar.php
+++ resource/navbar.php
@@ -16,9 +16,9 @@
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
-      <li class="nav-item>">
+      <!-- <li class="nav-item>">
         <a class="nav-link" href="index.php">Home</a>
-      </li>
+      </li> -->
       <li class="nav-item">
         <a class="nav-link" href="tabel_peminjaman.php">Peminjaman</a>
       </li>

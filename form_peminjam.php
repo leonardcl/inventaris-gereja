4cf83b1ee18ba420702bb06fd563bb00ecdaecb6
leonardcl <50519333+leonardcl@users.noreply.github.com>
2019-06-13 23:52:08 -0700

    test

--- form_peminjam.php
+++ form_peminjam.php
@@ -15,7 +15,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Inventaris</title>
+    <title>Inventaris test</title>
 
     <link rel='stylesheet' href='resource/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
 
